75544a5a4c9478dc02d172bd570eb773f3968995
gabrieladipoggio <gabrieladipoggio@gmail.com>
2022-04-07 17:24:12 -0300

    Adiciona seção 'como funciona' e 'mentoria' na Home

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  cards = [
+    {
+      icon: 'search',
+      text: 'Encontre um consultor'
+    },
+    {
+      icon: 'event',
+      text: 'Agende uma mentoria'
+    },
+    {
+      icon: 'question_answer',
+      text: 'Tire suas dúvidas'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
