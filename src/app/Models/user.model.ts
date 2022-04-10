624fee5e155dba43c7a7d10237c2c6744facd032
gabrieladipoggio <gabrieladipoggio@gmail.com>
2022-04-10 17:57:45 -0300

    Implementa funcionalidade de login

---
+++ src/app/Models/user.model.ts
@@ -0,0 +1,4 @@
+export class LoginDetails {
+  loginOuEmail: string = '';
+  senha:string = '';
+}
---
+++ src/app/Services/user.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginDetails } from '../Models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private username:string = '';
+
+  constructor(private http:HttpClient) {}
+
+  login(loginDetails:LoginDetails){
+    return this.http.post('http://localhost:8080/usuarios/verificacao-login', loginDetails, {responseType: 'text'}).subscribe(response => {
+      this.username = response
+      console.log(response)
+    },
+    (response) => {
+      console.log(response)
+    }
+    )
+  }
+
+  getUserName(){
+    return this.username
+  }
+}
