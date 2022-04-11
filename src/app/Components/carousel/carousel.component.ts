3049034d9334f25c875d779e7745ba9af71519c1
gabrieladipoggio <gabrieladipoggio@gmail.com>
2022-04-11 18:54:47 -0300

    Padroniza tamanho das imagens

--- src/app/Components/carousel/carousel.component.ts
+++ src/app/Components/carousel/carousel.component.ts
@@ -8,61 +8,15 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 })
 export class CarouselComponent implements OnInit {
   @Input() users:any = [];
-  // dynamicSlides = [
-  //   {
-  //     id: "1",
-  //     src:'/assets/img/foto-jorge.png',
-  //     nome:'Jorge Souza',
-  //     title: 'Dev Front-End',
-  //     skills: [
-  //       "Mobile", 'Angular', 'CSS', 'React', 'Svelte'
-  //     ]
-  //   },
-  //   {
-  //     id: "2",
-  //     src:'/assets/img/foto-julia.png',
-  //     nome:'Julia Garcia',
-  //     title: 'UX Designer',
-  //     skills: [
-  //       "Photoshop", 'Illustrator', 'Research', 'UX Writing', 'Figma'
-  //     ]
-  //   },
-  //   {
-  //     id: "3",
-  //     src:'/assets/img/foto-julia.png',
-  //     nome:'Julia Garcia',
-  //     title: 'UX Designer',
-  //     skills: [
-  //       "Mobile", 'Angular', 'CSS', 'React', 'Svelte'
-  //     ]
-  //   },
-  //   {
-  //     id: "4",
-  //     src:'/assets/img/foto-julia.png',
-  //     nome:'Julia Garcia',
-  //     title: 'UX Designer',
-  //     skills: [
-  //       "Mobile", 'Angular', 'CSS', 'React', 'Svelte'
-  //     ]
-  //   },
-  //   {
-  //     id: "6",
-  //     src:'/assets/img/foto-julia.png',
-  //     nome:'Julia Garcia',
-  //     title: 'UX Designer',
-  //     skills: [
-  //       "Mobile", 'Angular', 'CSS', 'React', 'Svelte'
-  //     ]
-  //   }
-  // ]
 
   constructor() { }
   customOptions: OwlOptions = {
     loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    autoHeight:false,
+    dots: true,
     navSpeed: 600,
     navText: ['&#8249', '&#8250'],
     responsive: {
