1fe80cf0992a6b997737eee46e45c6d155ec246c
gabrieladipoggio <gabrieladipoggio@gmail.com>
2022-04-12 13:59:11 -0300

    Implementa filtro de posts por categoria

--- src/app/Pages/forum/forum.component.ts
+++ src/app/Pages/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/Models/post.model';
 import { ForumService } from 'src/app/Services/forum.service';
 
 @Component({
@@ -10,11 +11,24 @@ export class ForumComponent implements OnInit {
 
   constructor(private forumService: ForumService) { }
 
-  posts: any[] = [];
+  posts:Post[] = [];
+  filteredPosts:Post[] = [];
 
   ngOnInit(): void {
     this.forumService.getAllPosts().subscribe((value:any) => {
-      this.posts = value;
+      // Mostrar os posts em ordem de mais recente para mais antigo
+      this.posts = value.reverse();
+      this.filteredPosts = this.posts;
      });
+  }
+
+  updateSelectedCategories(selectedCategories:string[]){
+    if(selectedCategories.length){
+      this.filteredPosts = this.posts.filter(post => {
+        return selectedCategories.includes(post.categoria)
+      });
+    } else {
+      this.filteredPosts = this.posts
+    }
   }
 }
